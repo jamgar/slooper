17ad6be6a72061b2ee088614a0562471e14c7e61
github username <github email address>
2023-03-21 20:56:48 -0500

    add caching

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
+  after_create :expire_cache
+  after_destroy :expire_cache
+
   validates :first_name, presence: true
   validates :email, presence: true, uniqueness: true, email: true
 
+ def expire_cache
+   ActionController::Base.new.expire_fragment('table_of_users')
+ end
 end
