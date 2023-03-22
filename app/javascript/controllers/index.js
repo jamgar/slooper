b8274bf22a9b378e61ac5dccb97e275a67f9783a
github username <github email address>
2023-03-22 07:17:22 -0500

    removed hello stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,8 +7,5 @@ import { application } from "./application"
 import FlashController from "./flash_controller"
 application.register("flash", FlashController)
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
 import SearchFormController from "./search_form_controller"
 application.register("search-form", SearchFormController)
--- app/services/user_delete_service.rb
+++ app/services/user_delete_service.rb
@@ -9,15 +9,3 @@ class UserDeleteService
     @user.destroy
   end
 end
-
-# Path: spec/services/user_delete_service_spec.rb
-# require 'rails_helper'
-
-# RSpec.describe UserDeleteService do
-#   let(:user) { create(:user) }
-
-#   it 'deletes the user' do
-#     expect { described_class.new(user).call }.to change { User.count }.by(-1)
-#   end
-#   
-# end
